9e03987ec086963f52821964ee38cae2cb433b71
Cian O'Ruanaidh <cianor.social@gmail.com>
2019-03-13 15:00:00 -0400

    Updates

---
+++ precache-manifest.6a632b7366b7f9cb3789cb7b932072fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b8c994c623cbace39f6",
+    "url": "/bike-share-app/static/css/main.a239f862.chunk.css"
+  },
+  {
+    "revision": "7b8c994c623cbace39f6",
+    "url": "/bike-share-app/static/js/main.7b8c994c.chunk.js"
+  },
+  {
+    "revision": "b37cec428c5a19bfd826",
+    "url": "/bike-share-app/static/js/runtime~main.b37cec42.js"
+  },
+  {
+    "revision": "2f4eaef981f5abf04b40",
+    "url": "/bike-share-app/static/js/2.2f4eaef9.chunk.js"
+  },
+  {
+    "revision": "19fabbdf71677064ad82e439bc585148",
+    "url": "/bike-share-app/static/media/logo.19fabbdf.png"
+  },
+  {
+    "revision": "633e74dc00ed9e4f04353568590b54e5",
+    "url": "/bike-share-app/static/media/racePointsImage.633e74dc.jpg"
+  },
+  {
+    "revision": "c6c00cabd57368b27ef9fd57f7c75f61",
+    "url": "/bike-share-app/static/media/bixiCity.c6c00cab.jpg"
+  },
+  {
+    "revision": "0b13064ecc5a0a030b4c19a327023336",
+    "url": "/bike-share-app/index.html"
+  }
+];
